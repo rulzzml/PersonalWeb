3e2933696153a934eb4279efce9fd46f3dd81e6a
rulzzml <139113956+rulzzml@users.noreply.github.com>
2023-12-04 17:23:48 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,75 +1,66 @@
-// Toggle class active untuk hamburger menu
-const navbarNav = document.querySelector('.navbar-nav');
-// ketika hamburger menu di klik
-document.querySelector('#hamburger-menu').onclick = () => {
-  navbarNav.classList.toggle('active');
-};
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Password geneator</title>
+    <link rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css"/>
+  </head>
+  <body>
+    <div class="container d-flex justify-content-center align-items-center
+      min-vh-100">
+      <div class="d-flex flex-column">
+        <h1>Buat Password anda</h1>
+        <div class="d-flex flex-column gap-2 mt-2">
+          <label for="passwordLength" class="text-uppercase fw-bold">Panjang
+            Password</label>
+          <input class="form-control form-control-lg" id="passwordLength"
+            type="number"/>
+          <label for="password" class="text-uppercase fw-bold">Password</label>
+          <input class="form-control form-control-lg" id="password"
+            disabled/>
+          <button class="btn btn-dark text-uppercase fw-bold btn-lg mt-2"
+            onclick="getPassword()">generate
+            password</button>
+          <a class="btn btn-primary text-uppercase fw-bold btn-lg mt-2"
+            onclick="savePassword()" id="saveButton">SAVE
+            PASSWORD</a>
+        </div>
+      </div>
+    </div>
+    <script>
+      let passwordLength = document.getElementById("passwordLength")
+let password = document.getElementById("password")
+let saveButton = document.getElementById("saveButton")
 
-// Toggle class active untuk search form
-const searchForm = document.querySelector('.search-form');
-const searchBox = document.querySelector('#search-box');
+const generatePassword = (len) => {
+  const lowerAlphabet = "abcdefghijklmnopqrstuvwxyz"
+  const upperAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  const numeric = "0123456789"
+  const symbol = "!@#$%^&*()_+=-{}[]';:/?.,<>~`"
 
-document.querySelector('#search-button').onclick = (e) => {
-  searchForm.classList.toggle('active');
-  searchBox.focus();
-  e.preventDefault();
-};
-
-// Toggle class active untuk shopping cart
-const shoppingCart = document.querySelector('.shopping-cart');
-document.querySelector('#shopping-cart-button').onclick = (e) => {
-  shoppingCart.classList.toggle('active');
-  e.preventDefault();
-};
-
-// Klik di luar elemen
-const hm = document.querySelector('#hamburger-menu');
-const sb = document.querySelector('#search-button');
-const sc = document.querySelector('#shopping-cart-button');
-
-document.addEventListener('click', function (e) {
-  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
-    navbarNav.classList.remove('active');
+  const data = lowerAlphabet + upperAlphabet + numeric + symbol
+  let generator = '';
+  for (let index = 0; index < len; index++) {
+    generator += data[~~(Math.random() * data.length)];
   }
+  return generator
+}
 
-  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
-    searchForm.classList.remove('active');
-  }
+const getPassword = () => {
+  const newPassword = generatePassword(passwordLength.value)
+  password.value = newPassword
+}
 
-  if (!sc.contains(e.target) && !shoppingCart.contains(e.target)) {
-    shoppingCart.classList.remove('active');
-  }
-});
-
-// Modal Box
-const itemDetailModal = document.querySelector('#item-detail-modal');
-const itemDetailButtons = document.querySelectorAll('.item-detail-button');
-
-itemDetailButtons.forEach((btn) => {
-  btn.onclick = (e) => {
-    itemDetailModal.style.display = 'flex';
-    e.preventDefault();
-  };
-});
-
-// klik tombol close modal
-document.querySelector('.modal .close-icon').onclick = (e) => {
-  itemDetailModal.style.display = 'none';
-  e.preventDefault();
-};
-
-// klik di luar modal
-window.onclick = (e) => {
-  if (e.target === itemDetailModal) {
-    itemDetailModal.style.display = 'none';
-  }
-};
-
-
-// loading
-const menuIcon = document.getElementById("menu-icon");
-const menuList = document.getElementById("menu-list");
-
-menuIcon.addEventListener("click", () => {
-  menuList.classList.toggle("hidden");
-});
+const savePassword = () => {
+  document.title = password.value
+  saveButton.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(`password saya: ${document.title}`))
+  saveButton.setAttribute('download', 'MyPasswordGeneratorLOG.txt')
+}
+    </script>
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"></script>
+  </body>
+</html>
